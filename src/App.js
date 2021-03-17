3d36f627798da538acacd19841964f2de12a8670
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-03-17 12:14:26 +0000

    Added React hooks and Semantic React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    //Create states for location
+    const [lat, setLat] = useState([]);
+    const [lon, setLon] = useState([]);
+    //Load functions once the app has reloaded
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            setLat(position.coords.latitude);
+            setLon(position.coords.longitude);
+        });
+        console.log("Latitude and longitude = ", lat, lon)
+    }, [lat, lon]);
+    return (
+        <div className="App">
+              
+        </div>
+    );
 }
-
-export default App;
