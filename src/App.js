adb706f5ca1511226fc7684146f9196f0a765814
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-03-18 16:35:59 +0000

    Hooray for paywalls
    It turns out OpenWeatherMap charges for SSL support, so I'm out of luck.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
+//import Weather from './components/weather';
 
+const { REACT_APP_API_KEY, REACT_APP_API_URL } = process.env;
+console.log("URL: " + REACT_APP_API_URL);
 export default function App() {
-    //Create states for location
+    //Create states with associated functions for location
     const [lat, setLat] = useState([]);
     const [lon, setLon] = useState([]);
-    //Load functions once the app has reloaded
+    const [data, setData] = useState([]);
+    //Load the state functions once the app has reloaded
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            setLat(position.coords.latitude);
-            setLon(position.coords.longitude);
-        });
-        console.log("Latitude and longitude = ", lat, lon)
-    }, [lat, lon]);
+        const fetchData = async() => {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                setLat(position.coords.latitude);
+                setLon(position.coords.longitude);
+            });
+            await fetch(`http://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${lon}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }})
+            .then(res => res.json())
+            .then(result => {
+                setData(result)
+                console.log(result);
+            });
+        }
+        fetchData();
+    }, [lat, lon])
     return (
         <div className="App">
               
