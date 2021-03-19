17b5ed3e7c737e9822f1a14e3244b7442f858140
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-03-19 15:14:44 +0000

    More UI tweaks

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import '../styles.css';
-import { Card } from 'semantic-ui-react';
 import * as moment from 'moment';
 
 const WeatherCard = ({ weatherData }) => (
     <div className="main">
         <p className="header">{weatherData.name}</p>
         <div className="flex">
-            <p className="day">Day: {moment().format('dddd')}</p>
-            <p className="day">{moment().format('LL')}</p>
+            <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
+            <p className="description">{weatherData.weather[0].main}</p>
         </div>
         <div className="flex">
             <p className="temp">Temp: {weatherData.main.temp} &deg;C</p>
             <p className="temp">Humidity: {weatherData.main.humidity}%</p>
+        </div>
+<div className="flex">
+            <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString()}</p>
+            <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}</p>
         </div>
     </div>
 )
